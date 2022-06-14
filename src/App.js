e831f825bead2c5e66fec7156e5dc09e56b5e7c8
Kevin Heaton <kevin.heaton21@yahoo.com>
2022-06-14 09:42:56 -0500

    added tests for About

--- src/App.js
+++ src/App.js
@@ -7,9 +7,9 @@ import Nav from './components/Nav';
 function App() {
   return (
     <div>
-      <Nav></Nav>
+      <Nav />
       <main>
-        <About></About>
+        <About />
       </main>
     </div>
   );
